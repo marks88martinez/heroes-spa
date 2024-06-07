38d8b0be800781937f0f6374a244d9f63176cac1
marcos <marks88martinez@gmail.com>
2024-06-06 23:14:36 -0400

    context

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
 import {  useNavigate } from "react-router-dom"
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
 
+
+    const { login } = useContext(AuthContext)
     const navigate = useNavigate();
     const onLogin = () => {
+
+      login('Marcos Martinez');
+
+
         navigate('/',{
             replace: true
         })
