011b606a2d0f9ea5d441fbf47b72e8d9982608de
marcos <marks88martinez@gmail.com>
2024-06-07 06:01:14 -0400

    login

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -8,20 +8,33 @@ const initialState = {
     logged: false
 }
 
+const init = () => {
+    const user = JSON.parse( localStorage.getItem('user'))
+
+    return {
+        logged: !!user,
+        user: user
+    }
+}
+
 export const AuthProvider = ({children}) => {
 
 
 
-    const [ authState, dispatch ] = useReducer(authReducer, initialState);
+    const [ authState, dispatch ] = useReducer(authReducer, initialState, init);
 
-    const login = (name = '')=>{
+    
+    const login = (name = '')=>{ 
+        
+        const user = {id: 'ABC', name}
+
         const  action = {
             type: types.login,
-            payload: {
-                id: 'abc',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify( user ) );
+
         dispatch(action)
     }
 
